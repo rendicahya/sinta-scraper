82bf96c2c0fc365248090163f1182ccc951d0192
Randy Cahya Wihandika <rendicahya@gmail.com>
2022-07-08 06:12:38 +0700

    Refactoring.

--- sinta/affiliation.py
+++ sinta/affiliation.py
@@ -15,34 +15,33 @@ def affiliation(affiliation_ids, output_format='dict'):
 def worker(affiliation_id, worker_result, *args, **kwargs):
     domain = get_config()['domain']
     url = f'{domain}/affiliations/profile/{affiliation_id}'
-    html = get(url)
-    soup = BeautifulSoup(html.content, 'html.parser')
+    html = BeautifulSoup(get(url).content, 'html.parser')
 
     # profile section
-    name = soup.select('.univ-name > h3')[0].text.strip()
-    abbrv_name = soup.select('.affil-abbrev')[0].text.strip()
-    location = soup.select('.affil-loc')[0].text.strip()
-    code = soup.select('.affil-code')[0].text.split(':')[-1].strip()
+    name = html.select('.univ-name > h3')[0].text.strip()
+    abbrv_name = html.select('.affil-abbrev')[0].text.strip()
+    location = html.select('.affil-loc')[0].text.strip()
+    code = html.select('.affil-code')[0].text.split(':')[-1].strip()
 
     # stats section
-    stat_profile = soup.select('.affil-profile-card .stat-num')
+    stat_profile = html.select('.affil-profile-card .stat-num')
     stat_names = 'authors', 'departments', 'journals'
     stat_int = [cast(stat_profile[i].text.replace('.', '')) for i in range(3)]
     stats = dict(zip(stat_names, stat_int))
 
     # sinta score section
-    score_soup = soup.select('.stat-profile .pr-num')
+    score_soup = html.select('.stat-profile .pr-num')
     score_names = 'overall', '3_years', 'productivity', 'productivity_3_years'
     score_numbers = [cast(score_soup[i].text.replace('.', '')) for i in range(4)]
     sinta_scores = dict(zip(score_names, score_numbers))
 
     # indexer section
     index_stats = {}
-    index_rows = soup.select('.stat-table > tbody > tr')
+    index_rows = html.select('.stat-table > tbody > tr')
     index_aspects = 'articles', 'citations', 'cited_documents', 'citation_per_researcher'
     indexers = 'scopus', 'scholar', 'wos', 'garuda'
 
-    last_update = soup.select('small')[-1].text.split(' :')[1]
+    last_update = html.select('small')[-1].text.split(' :')[1]
 
     for i, row in enumerate(index_rows):
         numbers = [cast(row.select('td')[i].text.replace('.', '').replace(',', '.')) for i in range(1, 5)]
