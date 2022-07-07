25aeef28aac2053b0f534773efb0634bcb043950
Randy Cahya Wihandika <rendicahya@gmail.com>
2022-07-08 06:08:53 +0700

    Adding `singlify()` function.

--- util/utils.py
+++ util/utils.py
@@ -35,11 +35,15 @@ def cast(string: str):
 
 
 def listify(val):
-    return [val] if type(val) not in [list, tuple] else val
+    return [val] if type(val) not in (list, tuple) else val
 
 
 def compact_list(_list):
     return _list[0] if len(_list) == 1 else _list
+
+
+def singlify(val):
+    return val[0] if type(val) in (list, tuple) else val
 
 
 def run_thread(worker, iterable, *args, **kwargs):
